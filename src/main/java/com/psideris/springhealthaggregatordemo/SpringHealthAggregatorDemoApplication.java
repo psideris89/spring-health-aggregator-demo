8110419fca22e8b1f6004d7c1c2aab32195f9a71
psideris <psideris1989@gmail.com>
2021-06-26 12:32:51 +0100

    Create spring boot project and install gradle 7.1

---
+++ src/main/java/com/psideris/springhealthaggregatordemo/SpringHealthAggregatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.psideris.springhealthaggregatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHealthAggregatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHealthAggregatorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psideris/springhealthaggregatordemo/SpringHealthAggregatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psideris.springhealthaggregatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHealthAggregatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
